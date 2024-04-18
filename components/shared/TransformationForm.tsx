ac4fe9a2f5531ac56cfe73de8b1da0147754b22c
Zheyuan Gao <gao.1600@osu.edu>
2024-04-17 20:42:10 -0500

    Implement the 5 features of the application

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -1,17 +1,16 @@
 "use client"
  
-import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import { z } from "zod"
 
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
-  
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -26,12 +25,17 @@ import {
 import { Input } from "@/components/ui/input"
 import { aspectRatioOptions, creditFee, defaultValues, transformationTypes } from "@/constants"
 import { CustomField } from "./CustomField"
-import { useState, useTransition } from "react"
+import { useEffect, useState, useTransition } from "react"
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils"
-import { set } from "mongoose"
-import { updateCredits } from "@/lib/actions/user.actions"
 import MediaUploader from "./MediaUploader"
 import TransformedImage from "./TransformedImage"
+import { updateCredits } from "@/lib/actions/user.actions"
+import { getCldImageUrl } from "next-cloudinary"
+import { addImage, updateImage } from "@/lib/actions/image.actions"
+import { useRouter } from "next/navigation"
+import { InsufficientCreditsModal } from "./InsufficientCreditsModal"
+import { set } from "mongoose"
+//import { InsufficientCreditsModal } from "./InsufficientCreditsModal"
  
 export const formSchema = z.object({
     title: z.string(),
@@ -53,6 +57,7 @@ const TransformationForm = ( { action, data = null, userId,
         const [isTransforming, setIsTransforming] = useState(false)
         const [transformationConfig, setTransformationConfig] = useState(config);
         const [isPending, startTransition] = useTransition()
+				const router = useRouter()
 
         const initialValues = data && action === 'Update' ? {
             title: data?.title,
@@ -69,10 +74,70 @@ const TransformationForm = ( { action, data = null, userId,
         })
         
         // 2. Define a submit handler.
-        function onSubmit(values: z.infer<typeof formSchema>) {
-            // Do something with the form values.
-            // ✅ This will be type-safe and validated.
-            console.log(values)
+        async function onSubmit(values: z.infer<typeof formSchema>) {
+            setIsSubmitting(true);
+
+            if (data || image){
+                const transformationUrl = getCldImageUrl({
+									width: image?.width,
+									height: image?.height,
+									src: image?.publicId,
+									...transformationConfig
+								})
+
+								const imageData = {
+									title: values.title,
+									publicId: image?.publicId,
+									transformationType: type,
+									width: image?.width,
+									height: image?.height,
+									config: transformationConfig,
+									secureURL: image?.secureURL,
+									transformationURL: transformationUrl,
+									aspectRatio: values.aspectRatio,
+									prompt: values.prompt,
+									color: values.color,
+								}
+
+								if (action === 'Add'){
+									try{
+										const newImage = await addImage({
+											image: imageData,
+											userId,
+											path: '/'
+										})
+
+										if(newImage){
+											form.reset()
+											setImage(data)
+											router.push(`/transformations/${newImage._id}`)
+										}
+									} catch (error){
+										console.log(error)
+									}
+								}
+
+								if (action === 'Update'){
+									try {
+										const updatedImage = await updateImage({
+											image: {
+												_id: data._id,
+												...imageData
+											},
+											userId,
+											path: `/transformation/${data._id}`
+										})
+
+										if(updatedImage){
+											router.push(`/transformations/${updatedImage._id}`)
+										}
+									} catch (error) {
+										console.log(error);
+									}
+								}
+            }
+
+					setIsSubmitting(false)
         }
 
         const onSelectFieldHandler = (value: string, 
@@ -120,9 +185,16 @@ const TransformationForm = ( { action, data = null, userId,
           })
         }
 
+				useEffect(() => {
+					if(image && (type === 'restore' || type === 'removeBackground')) {
+						setNewTransformation(transformationType.config)
+					}
+				}, [image, transformationConfig, type])
+
         return (
             <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                {creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />}
                 <CustomField 
                     control={form.control}
                     name="title"
@@ -141,8 +213,7 @@ const TransformationForm = ( { action, data = null, userId,
                             <Select
                                 onValueChange={(value) =>
                                 onSelectFieldHandler(value, field.onChange)}
-                                value={field.value}
-                            
+                                value={field.value}                            
                             >
                                 <SelectTrigger className="select-field">
                                     <SelectValue placeholder="Select size" />
